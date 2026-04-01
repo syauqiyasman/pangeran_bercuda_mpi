d738f346130da7202ee6a290909bdc6be3045628
Syauqi Yasman <146733124+syauqiyasman@users.noreply.github.com>
2026-04-01 15:40:15 +0700

    perbaiki script cluster T1 dan T2

--- cluster/1-run-hello_mpi.sh
+++ cluster/1-run-hello_mpi.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-#SBATCH -N 5
-#SBATCH --nodelist=node-01,node-03,node-06,node-07,node-08
+#SBATCH -N 4
+#SBATCH --ntasks-per-node=8
 
 mkdir -p logs
 
 for NP in 2 4 8 16 32; do
   echo "Running with NP=$NP"
-  mpirun --mca btl_tcp_if_exclude docker0,lo -np $NP /home/user04/pangeran_bercuda_mpi/1-hello_mpi 2>&1 \
+  srun -n $NP /home/user04/pangeran_bercuda_mpi/1-hello_mpi 2>&1 \
     | tee logs/1-hello_mpi-results-NP${NP}.txt
   echo "===================="
 done
--- cluster/2-run-matmat.sh
+++ cluster/2-run-matmat.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-#SBATCH -N 5
-#SBATCH --nodelist=node-01,node-03,node-06,node-07,node-08
+#SBATCH -N 4
+#SBATCH --ntasks-per-node=8
 
 mkdir -p logs
 
 for N in 512 1024 2048 4096; do
   for NP in 2 4 8 16 32; do
     echo "Running N=$N with NP=$NP"
-    mpirun --mca btl_tcp_if_exclude docker0,lo -np $NP /home/user04/pangeran_bercuda_mpi/2-matmat $N 2>&1 \
+    srun -n $NP /home/user04/pangeran_bercuda_mpi/2-matmat $N 2>&1 \
       | tee logs/2-matmat-results-N${N}_NP${NP}.txt
     echo "===================="
   done
